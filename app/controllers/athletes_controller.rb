dd33a665ad974031b67678b6cba3ee432858af6e
Henry <hvelas93@gmail.com>
2021-04-20 10:33:05 -0400

    create functionality

--- app/controllers/athletes_controller.rb
+++ app/controllers/athletes_controller.rb
@@ -10,6 +10,18 @@ class AthletesController < ApplicationController
         render json: athlete
     end
 
+    def create
+        new_athlete = Athlete.create!(athlete_params)
+        render json: new_athlete
+    end
+
+    private
+
+    def athlete_params
+        params.permit(:email, :name, :password, :age, :weight_in_lbs)
+    end
+
+
     
     
 end
