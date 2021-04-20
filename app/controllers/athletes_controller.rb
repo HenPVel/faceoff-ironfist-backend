c04d42832b31b131679bc902e606d7590eec2b7b
Henry <hvelas93@gmail.com>
2021-04-20 10:41:16 -0400

    added athlete update functionality

--- app/controllers/athletes_controller.rb
+++ app/controllers/athletes_controller.rb
@@ -15,13 +15,28 @@ class AthletesController < ApplicationController
         render json: new_athlete
     end
 
-    private
+    def destroy
+        athlete = Athlete.find(params[:id])
+        athlete.destroy!
+        render json: {}
+    end
 
-    def athlete_params
-        params.permit(:email, :name, :password, :age, :weight_in_lbs)
+    def update
+        athlete = Article.find(params[:id])
+        athlete.update(athlete_update_params)
+        render json: athlete
     end
 
 
+    private
+
+    def athlete_sign_up_params
+        params.permit(:email, :name, :password, :age, :weight_in_lbs)
+    end
+
+    def athlete_update_params
+        params.permit(:email, :name, :age, :years_of_experience, :reason_for_training, :ethnicity, :body_type, :skill_level, :weight_in_lbs, :height_in_inches, :preexisting_injuries, :username, :password, :active )
+    end
     
     
 end
